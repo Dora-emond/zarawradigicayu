86a542822173f691d155f7db368f1cf603af9314
Dora-emond <85222479+Dora-emond@users.noreply.github.com>
2021-07-14 01:20:35 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,12 +1,17 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
+╭─「 Donasi • linkaja 」
+│ • hubungi owner ketik .owner]
+ |  • Saweria https://saweria.co/Doraemond
+ |   • Three [wa.me/089601145546]
+│ • Telkomsel [wa.me/6282387704390]
+ |  • Saya harap kamu menyukai bot saya
+ |      dan jangan lupa untuk mensuport saya
+ |     karena saya sangat menyukai dora
+ |     # by owner Dora :D
+ |       saya harap kamu bahagia
 ╰────
 
-╭─「 Donasi • Non Pulsa 」
-│ • Gopay, OVO, Dana [081515860089]
-│ • https://saweria.co/Nurutomo
+╭─「 Dorameond 
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
